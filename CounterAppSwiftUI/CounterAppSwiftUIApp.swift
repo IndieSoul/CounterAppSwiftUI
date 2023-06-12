c05da04a5f05dc91b93de47f46d9cd1f4f61b809
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2023-06-12 02:09:09 -0600

    Initial Commit

---
+++ CounterAppSwiftUI/CounterAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppSwiftUIApp.swift
+//  CounterAppSwiftUI
+//
+//  Created by Luis Enrique Rosas Espinoza on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
